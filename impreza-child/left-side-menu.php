4ad4ccb15c0e05c5a73db8b7ab034be801585219
Nathaniel Mantell <mantelln@Nathaniels-MacBook-Pro-2.local>
2025-10-26 21:57:30 -0400

    Add x button to nav menu

--- impreza-child/left-side-menu.php
+++ impreza-child/left-side-menu.php
@@ -12,6 +12,7 @@ $menu_html = wp_nav_menu([
 <!--button id="lsm-open" aria-haspopup="true" aria-controls="lsm-panel" aria-expanded="false">Menu</button-->
 
 <aside id="lsm-panel" class="lsm-panel" aria-hidden="true">
+    <button id="lsm-close" aria-label="Close menu">✕</button>
     <div class="lsm-nav-logo-container">
         <img class="lsm-nav-logo" height="80" src="https://development-website.online/wp-content/uploads/Bongo-Tech-Logo.webp" class="attachment-full size-full" alt="" loading="lazy" decoding="async" srcset="https://development-website.online/wp-content/uploads/Bongo-Tech-Logo.webp 599w, https://development-website.online/wp-content/uploads/Bongo-Tech-Logo-300x88.webp 300w" sizes="auto, (max-width: 599px) 100vw, 599px">
     </div>
